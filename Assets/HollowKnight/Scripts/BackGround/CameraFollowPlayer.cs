177813716f16918e745342c1800b9655eb31019e
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2024-11-23 16:28:33 +0700

    Use cinemachine virtual camera

--- Assets/HollowKnight/Scripts/BackGround/CameraFollowPlayer.cs
+++ Assets/HollowKnight/Scripts/BackGround/CameraFollowPlayer.cs
@@ -3,18 +3,6 @@ using UnityEngine;
 
 public class CameraFollowPlayer : ParentBehavior
 {
-   
-    public Transform player; // Transform của nhân vật chính
-    [SerializeField]  Vector3 offSet = new Vector3(0, 3, 0);
-    protected override void LoadExternalComponent()
-    {
-        base.LoadExternalComponent();
-        if(player == null) player = GameObject.Find("Player").transform;
-    }
-
-    private void LateUpdate()
-    {
-        if (player == null) return;
-        transform.position = player.position + offSet;
-    }
 }
+
+
